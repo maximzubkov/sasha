da06670b9d8e424ba17be444910e2611a0c7b5f4
maximzubkov <zubkov.md@phystech.edu>
2019-09-27 22:54:01 +0300

    data parsing

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -12,8 +12,8 @@ class Home extends React.Component {
 		constructor(props){
 			super(props);
 			this.state = {
-					qrData: 111,
-					qrType: 111,
+					qrData: null,
+					qrType: null,
 			};
 		}
 
