53e4358939a1220709d522b1624971f9248e8d0e
maximzubkov <zubkov.md@phystech.edu>
2019-09-27 19:37:11 +0300

    Updates

---
+++ precache-manifest.fd070557585bc40cfea424f99b6dd2ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2780d97e69a1a3bf1e9785f679a1d02c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "90eb6413248ce72ee72a",
+    "url": "./static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "a4779522f7ab3d82a2b1",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "90eb6413248ce72ee72a",
+    "url": "./static/js/2.384624bd.chunk.js"
+  },
+  {
+    "revision": "a4779522f7ab3d82a2b1",
+    "url": "./static/js/main.4956d606.chunk.js"
+  },
+  {
+    "revision": "f075708bee7cfea9be74",
+    "url": "./static/js/runtime-main.6dfb5a71.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3f4312052d20cd3cdf4b50a0c33f2ec3.js"
+  "./precache-manifest.fd070557585bc40cfea424f99b6dd2ab.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
