6fa4800da51e515ebd618c6a9a7d8be652050b9e
maximzubkov <zubkov.md@phystech.edu>
2019-09-27 19:34:49 +0300

    Updates

---
+++ precache-manifest.3f4312052d20cd3cdf4b50a0c33f2ec3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d6e1988250a99d8d366c44737191242",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4a1ff77eb7129132dc7",
+    "url": "/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "a4779522f7ab3d82a2b1",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "a4a1ff77eb7129132dc7",
+    "url": "/static/js/2.2ee6d15e.chunk.js"
+  },
+  {
+    "revision": "a4779522f7ab3d82a2b1",
+    "url": "/static/js/main.4956d606.chunk.js"
+  },
+  {
+    "revision": "d12967902325f89d5c3a",
+    "url": "/static/js/runtime-main.61a899d4.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
