1ac30c710b866488503d80ca40226d0ee5113161
maximzubkov <zubkov.md@phystech.edu>
2019-09-29 05:08:20 +0300

    audio bar

--- src/audio/songs.js
+++ src/audio/songs.js
@@ -1,5 +1,30 @@
 export default [
   {
+    url: 'https://media.izi.travel/fae0d384-5475-4134-a0bf-eab6bbf42a1b/11df9a42-ec7f-4bde-b4a1-219e8956952b.m4a',
+    title: 'Белые кувшинки',
+    duration: 96
+  },
+  {
+    url: 'https://media.izi.travel/fae0d384-5475-4134-a0bf-eab6bbf42a1b/b12c8091-b922-4672-809d-797a91d34154.m4a',
+    title: 'Пьеро и Арлекин ',
+    duration: 181
+  },
+  {
+    url: 'https://media.izi.travel/fae0d384-5475-4134-a0bf-eab6bbf42a1b/10c997f2-e12b-4d03-9b59-a9d1949bc125.m4a',
+    title: 'Христос и грешница',
+    duration: 212
+  },
+  {
+    url: 'https://media.izi.travel/fae0d384-5475-4134-a0bf-eab6bbf42a1b/4a966c10-8178-469f-9865-ee4992e7144d.m4a',
+    title: 'Голова ангела',
+    duration: 130
+  },
+  {
+    url: 'https://media.izi.travel/fae0d384-5475-4134-a0bf-eab6bbf42a1b/80f2472d-7e2a-4119-ad17-3e34c9fbe20e.m4a',
+    title: 'Madonna',
+    duration: 110
+  },
+  {
     url: 'https://media.izi.travel/fae0d384-5475-4134-a0bf-eab6bbf42a1b/23e363df-3364-49ca-b0ad-d3216c444d97.m4a',
     title: 'Venera',
     duration: 122
